5885f6b7b43259080ede0397bb9e66fe8825c77c
thanky1810 <thanky4444@gmail.com>
2026-01-04 23:44:07 +0700

    day 3 java Learn data types

--- Initial.java
+++ Initial.java
@@ -1,5 +1,11 @@
 class Initial {
     public static void main(String[] args) {
-        System.out.println(2*5);
+        String name = "Than van Ky";
+        System.out.println("Hello " + name);
+        int age = 20;
+        System.out.println("I am " + age + " years old");
+
+        final double PI = 3.14; // constant variable
+        System.out.println("Value of PI: " + PI);
     }
 }
